b832c4551916b7fc7793da07362cca92987ad58e
zhelez9aca <azeleznov95@gmail.com>
2026-05-02 16:18:29 +0300

    building problems was fixed

--- src/main/java/ru/shortener/controller/ShortLinkController.java
+++ src/main/java/ru/shortener/controller/ShortLinkController.java
@@ -7,46 +7,37 @@ import ru.shortener.dto.ShortLinkResponse;
 import ru.shortener.dto.ShortLinkStatsResponse;
 import ru.shortener.dto.UrlRequest;
 import ru.shortener.model.ShortLink;
-import ru.shortener.service.ShortLinkService;
 import ru.shortener.config.AppProperties;
+import ru.shortener.service.ShortLinkService;
+
 @RestController
 public class ShortLinkController {
     private final ShortLinkService service;
     private final AppProperties appProperties;
-    ShortLinkController(AppProperties appProperties, ShortLinkService service){
+
+    ShortLinkController(AppProperties appProperties, ShortLinkService service) {
         this.service = service;
         this.appProperties = appProperties;
-
-        
     }
 
     @GetMapping("/stats/{code}")
-    public ShortLinkStatsResponse getStats(@PathVariable String code){
+    public ShortLinkStatsResponse getStats(@PathVariable String code) {
         return service.getStats(code);
-
     }
+
     @PostMapping("/shorten")
-    public ShortLinkResponse createShortLink(@RequestBody @Valid UrlRequest urlRequest){
+    public ShortLinkResponse createShortLink(@RequestBody @Valid UrlRequest urlRequest) {
         String url = urlRequest.getUrl();
         ShortLink shortLink = service.createShortLink(url);
         String baseUrl = appProperties.getBaseurl();
-        String clientUrl = baseUrl+"/"+shortLink.getShortcode();
+        String clientUrl = baseUrl + "/" + shortLink.getShortcode();
         ShortLinkResponse clientUrlResponse = new ShortLinkResponse(clientUrl);
         return clientUrlResponse;
     }
-    @GetMapping("/{code}")
-    public ResponseEntity<Void> getShortLink(@PathVariable String code){
+
+    @GetMapping("/{code:[0-9A-Za-z]{9}}")
+    public ResponseEntity<Void> getShortLink(@PathVariable String code) {
         String originalUrl = service.getOriginalUrl(code);
         return ResponseEntity.status(307).header("Location", originalUrl).build();
-
-
     }
-    @GetMapping("/stats/{code}")
-    public ShortLinkStatsResponse getStats(@PathVariable String code){
-        return service.getStats(code);
-
-
-    }
-
-
 }
--- src/main/java/ru/shortener/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/shortener/exception/GlobalExceptionHandler.java
@@ -1,16 +1,8 @@
 package ru.shortener.exception;
 
-
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import ru.shortener.dto.ErrorResponse;
-import ru.shortener.exceptons.InvalidUrlException;
-import ru.shortener.exceptons.ShortLinkNotFoundException;
-
-import java.time.LocalDateTime;
-
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.shortener.dto.ErrorResponse;
 import ru.shortener.exceptons.InvalidUrlException;
@@ -20,42 +12,31 @@ import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(InvalidUrlException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidUrlException(InvalidUrlException ex){
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setMessage(ex.getMessage());
-<<<<<<< HEAD
-        errorResponse.setTimestamp(LocalDateTime.now());
-        errorResponse.setStatus(400);
-        return ResponseEntity.status(400).body(errorResponse);
-    }
-    @ExceptionHandler(ShortLinkNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleShortLinkNotFoundException(ShortLinkNotFoundException ex){
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setMessage(ex.getMessage());
-        errorResponse.setTimestamp(LocalDateTime.now());
-        errorResponse.setStatus(404);
-        return ResponseEntity.status(404).body(errorResponse);
 
-    }
-    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+    @ExceptionHandler(InvalidUrlException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidUrlException(InvalidUrlException ex) {
         ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setStatus(400);
-        errorResponse.setTimestamp(LocalDateTime.now());
         errorResponse.setMessage(ex.getMessage());
-        return ResponseEntity.status(400).body(errorResponse);
-=======
         errorResponse.setStatus(400);
         errorResponse.setBrokeAt(LocalDateTime.now());
         return ResponseEntity.status(400).body(errorResponse);
     }
+
     @ExceptionHandler(ShortLinkNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidUrlException(ShortLinkNotFoundException ex){
+    public ResponseEntity<ErrorResponse> handleShortLinkNotFoundException(ShortLinkNotFoundException ex) {
         ErrorResponse errorResponse = new ErrorResponse();
         errorResponse.setMessage(ex.getMessage());
         errorResponse.setStatus(404);
         errorResponse.setBrokeAt(LocalDateTime.now());
         return ResponseEntity.status(404).body(errorResponse);
->>>>>>> 152fe1a (Stable backend: PostgreSQL + JPA + REST API working)
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setStatus(400);
+        errorResponse.setMessage(ex.getMessage());
+        errorResponse.setBrokeAt(LocalDateTime.now());
+        return ResponseEntity.status(400).body(errorResponse);
     }
 }
